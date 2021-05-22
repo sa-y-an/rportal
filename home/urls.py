df0b03a2e52a6fac229a7d3523563291838f38be
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-05-22 13:57:28 +0530

    all pages added

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.home , name= 'home')
+    path('', views.home , name= 'home'),
+    path('about/', views.about , name= 'about'),
+    path('director/', views.director , name= 'director'),
+    path('mile/', views.mile , name= 'mile'),
+    path('team/', views.team , name= 'team'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home/home.html')
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def director(request):
+    return render(request, 'home/director.html')
+
+def mile(request):
+    return render(request, 'home/mile.html')
+
+def team(request):
+    return render(request, 'home/team.html')
