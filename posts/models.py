a0fb9162052705f241bd44a7eb827c6e11cdfcd7
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-06-16 17:18:43 +0530

    added many to many

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from usr_val.models import Teacher
+from usr_val.models import Teacher, Student
 
 # Create your models here.
 # static questions containing only text and images
@@ -7,10 +7,11 @@ from usr_val.models import Teacher
 
 class Post(models.Model):
     title = models.CharField(blank=True, max_length=200)
-    description = models.TextField(blank=True, default='hello')
+    description = models.TextField(blank=True, default=' Will help your carrer')
     image_url = models.URLField(blank=True)
-    tag = models.TextField(blank=True, default='hint')
+    tag = models.TextField(blank=True, default='open to all')
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    student = models.ManyToManyField(Student, blank=True)
 
     def __str__(self):
         return self.title
