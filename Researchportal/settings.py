6dc5d53670f95f9ef04982875976bb79a07df9e1
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-06-19 02:22:44 +0530

    added auth

--- Researchportal/settings.py
+++ Researchportal/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://ieee-rportal1.herokuapp.com/']
 
