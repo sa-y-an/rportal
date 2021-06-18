1271e5861917e6b7ecf9b8fd05d2dd2f979ca517
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-06-19 02:08:12 +0530

    added forget password emails heroku

--- Researchportal/settings.py
+++ Researchportal/settings.py
@@ -139,3 +139,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+
+
+# Email Backend
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ieeesbnitd@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ['PASSWORD']
