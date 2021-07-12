24deb6ef3a361560e5b4ebfc44300c97054b70ff
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-07-13 00:22:54 +0530

    added screenshots and changed email prefferences

--- Researchportal/dev_settings.py
+++ Researchportal/dev_settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ieeesbnitd@gmail.com'
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("PASSWORD")
