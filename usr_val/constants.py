4770fce1cc1bcf4d60b2e14a57c66166e469e22d
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-07-16 01:42:41 +0530

    reformatted

--- usr_val/constants.py
+++ usr_val/constants.py
@@ -1,17 +1,17 @@
 # Departments
-DEPARTMENTS=[
-    ('BT','BT'),
+DEPARTMENTS = [
+    ('BT', 'BT'),
     ('CE', 'CE'),
     ('CH', 'CH'),
-    ('CS','CH'),
-    ('CY','CY'),
-    ('EC','EC'),
-    ('EE','EE'),
-    ('ES','ES'),
-    ('HS','HS'),
-    ('MA','MA'),
-    ('ME','ME'),
-    ('MM','MM'),
-    ('MS','MS'),
-    ('PH','PH')
+    ('CS', 'CH'),
+    ('CY', 'CY'),
+    ('EC', 'EC'),
+    ('EE', 'EE'),
+    ('ES', 'ES'),
+    ('HS', 'HS'),
+    ('MA', 'MA'),
+    ('ME', 'ME'),
+    ('MM', 'MM'),
+    ('MS', 'MS'),
+    ('PH', 'PH')
 ]
