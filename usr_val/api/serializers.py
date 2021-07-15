d3d1087ba493b291af9647be334fd9594daf2620
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-07-15 19:50:55 +0530

    initial commit

---
+++ usr_val/api/serializers.py
@@ -0,0 +1,101 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.models import User
+
+from usr_val.utils import institute_email_validator, LowerEmailField, FileValidator
+from usr_val.models import Teacher, Student
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    email = LowerEmailField(
+        required=True,
+        allow_blank=False,
+        label='Email address',
+        max_length=30,
+        validators=[UniqueValidator(queryset=User.objects.all()), institute_email_validator],
+    )
+    first_name = serializers.CharField(
+        required=True,
+        max_length=60,
+        allow_blank=False
+    )
+    last_name = serializers.CharField(
+        required=False,
+        max_length=60,
+        allow_blank=True,
+        default=''
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({'confirm_password': 'Passwords must match'})
+        account = User(
+            username=self.validated_data['username'],
+            email=self.validated_data['email'].lower(),
+            first_name=self.validated_data['first_name'],
+            last_name=self.validated_data['last_name'],
+            is_active=True  # TO BE CHANGED TO FALSE
+        )
+        account.set_password(password)
+        account.save()
+        # send email from here
+        return account
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'groups','id']
+
+
+class StudentRegistrationSerializer(serializers.ModelSerializer):
+    cv = serializers.FileField(allow_null=True,
+                               max_length=100,
+                               required=False,
+                               use_url=True,
+                               validators=[FileValidator(content_types=('application/pdf',),max_size=1024*1024)]
+                               )
+
+    class Meta:
+        model=Student
+        exclude=['user',]
+
+    def save(self):
+        try:
+            request=self.context.get('request')
+            user=request.user
+        except Exception as e:
+            raise serializers.ValidationError('Could not get user')
+
+        if user.groups.first().name!='student':  # checks if the user is actually a student
+            raise serializers.ValidationError('Teacher cannot create Student profile.')
+
+        if Student.objects.filter(user=user).exists():
+            raise serializers.ValidationError('Profile already exists.')
+        student=Student(
+            user=user,
+            branch=self.validated_data['branch'],
+            contact=self.validated_data.get('contact'),
+            cv=self.validated_data.get('cv'),
+        )
+        student.save()
+        return student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=Student
+        fields='__all__'
+
+
