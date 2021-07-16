4a0f6bd421e059e314752128e09303a3e1c1cdc2
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-07-16 17:06:07 +0530

    Changed Contact Field
    Contact Field: Char
    Closes #9

--- usr_val/models.py
+++ usr_val/models.py
@@ -28,7 +28,7 @@ class Teacher(models.Model):
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     branch = models.CharField(max_length=4, choices=DEPARTMENTS)
-    contact = models.BigIntegerField(blank=True,null=True)
+    contact = models.CharField(blank=True,null=True, max_length=15)
     cv = models.FileField(null=True,
                           upload_to=cv_upload_location,
                           validators=[FileExtensionValidator(allowed_extensions=['pdf',])],
