e8f1e342af5777cfa4d201f7e4b4c4ae1e0c28a3
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-07-18 15:09:04 +0530

    added API schema

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-
+from rest_framework.schemas import get_schema_view
 
 app_name = 'home'
 
@@ -10,4 +10,14 @@ urlpatterns = [
     path('director/', views.director , name= 'director'),
     path('mile/', views.mile , name= 'mile'),
     path('team/', views.team , name= 'team'),
+
+    ## Api Documentation 
+
+    path('schema', get_schema_view(
+        title="RpBackendAPI",
+        description="API for the Research Portal",
+        version="1.0.0"
+    ), name='openapi-schema'),
+
+
 ]
