d32652c550963f8c5f74379347f4734df5f12c11
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-07-18 10:39:13 +0530

    Added perform_create to StudentRegistrationView

--- usr_val/api/views.py
+++ usr_val/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.generics import (
     CreateAPIView,
     ListAPIView,
 )
+from rest_framework.serializers import ValidationError
 from .serializers import (
     RegistrationSerializer,
     UserSerializer,
@@ -26,6 +27,20 @@ class StudentRegistrationView(CreateAPIView):
         context = super(StudentRegistrationView, self).get_serializer_context()
         context.update({"request": self.request})
         return context
+    
+    def perform_create(self,serializer):
+        try:
+            user = self.request.user
+        except Exception as e:
+            raise ValidationError('Could not get user')
+
+        if user.groups.first().name != 'student':  # checks if the user is actually a student
+            raise ValidationError('Teacher cannot create Student profile.')
+
+        if Student.objects.filter(user=user).exists():
+            raise ValidationError('Profile already exists.')
+
+        serializer.save(user=self.request.user)
 
 
 class TeacherRegistrationView(CreateAPIView):
