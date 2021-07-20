b73bb88646dcdac5a46bde8cdb94ee5bcc790ed6
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-07-21 01:36:12 +0530

    added URL for re-requesting verification mails

--- usr_val/api/urls.py
+++ usr_val/api/urls.py
@@ -7,6 +7,7 @@ from .views import (
     AllStudentsView,
     TeacherRegistrationView,
     AllTeachersView,
+    resendVerificationView,
 )
 from usr_val.views import activate
 app_name = 'usr_val'
@@ -14,6 +15,7 @@ app_name = 'usr_val'
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='user_registration'),
     path('list/', AllUsersView.as_view(), name='all_users_list'),
+    path('resend-user-activation/', resendVerificationView, name='resend_verification'),
     path('student/create-profile/', StudentRegistrationView.as_view(), name='student_registration'),
     path('student/all/', AllStudentsView.as_view(), name='students'),
     path('teacher/create-profile/', TeacherRegistrationView.as_view(), name='teacher_registration'),
