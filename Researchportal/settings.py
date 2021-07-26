375103118a5ebdfeac4b5055ca207bf0dd45d8b1
Sayan Mondal <sayan.ee.nitd@gmail.com>
2021-07-26 21:54:24 +0530

    pythonanywhere baal

--- Researchportal/settings.py
+++ Researchportal/settings.py
@@ -126,24 +126,14 @@ WSGI_APPLICATION = 'Researchportal.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': env("DB_NAME"),
-
-        'USER': env("DB_USER"),
-
-        'PASSWORD': env("DB_PASSWORD"),
-
-        'HOST': env("DB_HOST"),
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+
+
 
 
 
