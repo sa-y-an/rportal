5d98d8fa682dd939ea05eb058c7161a899dedae1
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-07-29 21:58:56 +0530

    formatting changes

--- usr_val/api/urls.py
+++ usr_val/api/urls.py
@@ -16,15 +16,18 @@ from usr_val.views import activate
 app_name = 'usr_val'
 
 urlpatterns = [
+    # user related
     path('register/', RegistrationView.as_view(), name='user_registration'),
     path('list/', AllUsersView.as_view(), name='all_users_list'),
     path('resend-user-activation/',resendVerificationView,name='resend-user-activation'),
     path('details/<str:username>/',RetrieveUpdateUserView.as_view(),name='user_update'),
 
+    # student related
     path('student/create-profile/', StudentRegistrationView.as_view(), name='student_registration'),
     path('student/all/', AllStudentsView.as_view(), name='students'),
     path('student/details/<str:username>/', RetrieveUpdateStudentView.as_view(), name='student_update'),
 
+    # teacher related
     path('teacher/create-profile/', TeacherRegistrationView.as_view(), name='teacher_registration'),
     path('teacher/all/', AllTeachersView.as_view(), name='teachers'),
     path('teacher/details/<str:username>/', RetrieveUpdateTeacherView.as_view(), name='teacher_update'),
