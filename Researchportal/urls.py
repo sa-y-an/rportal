ab806f19ce1d62ad518d9036c5d82a9b0d42f6e1
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-07-31 18:30:36 +0530

    formatting changes

--- Researchportal/urls.py
+++ Researchportal/urls.py
@@ -9,12 +9,10 @@ from rest_framework_simplejwt.views import (
 from django.urls import re_path
 from django.views.static import serve
 
-
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
 
 from usr_val.api.views import MyTokenObtainPairView
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,35 +21,32 @@ urlpatterns = [
     # path('users/', include('usr_val.urls')),
 
     # API URLs
-    path('api/user/',include('usr_val.api.urls',namespace='user_api')),
+    path('api/user/', include('usr_val.api.urls', namespace='user_api')),
 
     # JWT
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('reset_password/',
-    auth_views.PasswordResetView.as_view(template_name="usr_val/password_reset.html"),
-    name="reset_password"),
+         auth_views.PasswordResetView.as_view(template_name="usr_val/password_reset.html"),
+         name="reset_password"),
 
     path('reset_password_sent/',
-    auth_views.PasswordResetDoneView.as_view(template_name="usr_val/password_reset_sent.html"),
-    name="password_reset_done"),
+         auth_views.PasswordResetDoneView.as_view(template_name="usr_val/password_reset_sent.html"),
+         name="password_reset_done"),
 
     path('reset/<uidb64>/<token>/',
-    auth_views.PasswordResetConfirmView.as_view(template_name="usr_val/password_reset_form.html"),
-    name="password_reset_confirm"),
+         auth_views.PasswordResetConfirmView.as_view(template_name="usr_val/password_reset_form.html"),
+         name="password_reset_confirm"),
 
     path('reset_password_complete/',
-    auth_views.PasswordResetCompleteView.as_view(template_name="usr_val/password_reset_done.html"),
-    name="password_reset_complete"),
+         auth_views.PasswordResetCompleteView.as_view(template_name="usr_val/password_reset_done.html"),
+         name="password_reset_complete"),
 
     # workaround for media
 
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-
-
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
     ## Api Documentation
 
