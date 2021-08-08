9ec70d00bbc3611a885786011e7c829030cedd76
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-08-08 17:50:47 +0530

    added DELETE option for projects

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.PostPublishedList.as_view(), name='feed'),
-    path('details/<slug:slug>/', views.ProjectRetrieveUpdateView.as_view(), name='post_detail'),
+    path('details/<slug:slug>/', views.ProjectRetrieveUpdateDestroyView.as_view(), name='post_detail'),
     path('create/', views.PostCreateView.as_view(), name='create_post'),
     path('apply/<slug:slug>/', views.apply_project, name='apply_post'),
     path('shortlist/<slug:slug>/', views.shortlistStudents, name='shortlist'),
